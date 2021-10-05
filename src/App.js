a017aea887f2b6eaa21166b2d44a9243bd4bbfcc
Denis <denis.tojaga@edu.fit.ba>
2021-10-05 17:21:32 +0200

    //Router implementation beginning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 //components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from "./components/Movie";
+import NotFound from './components/NotFound';
+
+
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 //styles
 import { GlobalStyle } from './style/GlobalStyle';
@@ -11,11 +18,16 @@ import { GlobalStyle } from './style/GlobalStyle';
 //we can either use the functions for creating the components or arrow functions
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+
+
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
